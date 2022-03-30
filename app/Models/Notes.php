943b0858a9e182e738ed6b9dcfda996a1f24d258
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-29 23:06:05 -0700

    Update Notes.php

--- app/Models/Notes.php
+++ app/Models/Notes.php
@@ -2,17 +2,12 @@
 
 namespace App\Models;
 
+protected $fillable = ['equipment_id', 'services', 'software', 'notes'];
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notes extends Model
 {
     use HasFactory;
-    protected $fillable = ['equipment_id','services', 'software','notes'];
-    protected $table = 'notes';
-
-    public function equipment()
-    {
-        return $this->belongto(Equipment::class);
-    }
 }
