bc67a109d18d5452864b96dd1e362f97fa41c232
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-30 00:00:34 -0700

    Update PurchasesController.php

--- app/Http/Controllers/PurchasesController.php
+++ app/Http/Controllers/PurchasesController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Purchases;
+use Kris\LaravelFormBuilder\FormBuilder;
+use App\Forms\PurchasesForm;
 
-class PurchasesController extends Controller
+class InvoiceController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -19,6 +21,40 @@ class PurchasesController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(FormBuilder $formBuilder, Request $request)
+    {
+        $form = $formBuilder->create(PurchasesForm::class, [
+            'method' => 'POST',
+            'url' => route('purchases.store'),
+        ]);
+        $user = $request->get('user', 0);
+        $form->modify('customer_id', "number", [
+            'default_value' => $user,
+        ]);
+        
+        return view('purchases.create', compact('form'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(FormBuilder $formBuilder)
+    {
+        $form = $formBuilder->create(PurchasesForm::class);
+        $form->redirectIfNotValid();
+        $purchases = Purchases::create($form->getFieldValues());
+        $purchases->equipment()->attach(1);
+        return $this->index();
+    }
+
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -26,10 +62,11 @@ class PurchasesController extends Controller
      */
     public function show($id)
     {
-        $purchases = Invoice::find($id);
-        $purchases->customer;
-        $purchases->equipment;
-        return json_encode(compact('purchases'));
+        $invoice = Invoice::find($id);
+        // Lazy Loading
+        $invoice->customer;
+        $invoice->equipments;
+        return json_encode(compact('invoice'));
     }
 
     /**
