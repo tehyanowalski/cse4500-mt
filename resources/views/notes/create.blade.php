b407e73fc69e2c54651ba5707a453fbedb09b072
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-29 23:36:46 -0700

    Update create.blade.php

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,16 +1,5 @@
 @extends('adminlte::page')
 
-@section('title', 'Notes')
-
-@section('content_header')
-    <h1>History</h1>
-@stop
-
 @section('content')
-<form method="post" action="{{ route('notes.store') }}" >
-    @csrf
-    <x-adminlte-input name="title" label="Title" />
-    <x-adminlte-input name="progress" type="number" min=0 max=100 label="Progress" />
-    <x-adminlte-button type="Submit" label="Submit" />
-</form>
-@stop
+    {!! form($form) !!}
+@endsection
