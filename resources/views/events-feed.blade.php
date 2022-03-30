8410f609c52a8f05780955e2a1a5f00cf3d38c25
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-29 18:12:27 -0700

    Delete events-feed.blade.php

--- resources/views/events-feed.blade.php
+++
@@ -1,5 +0,0 @@
-<?php
-array(["title":"CSE4500 Class","start":"2022-02-23T17:30:00","end":"2022-02-23T18:45:00"],["title":"CSE4500 Class","start":"2022-02-28T17:30:00","end":"2022-02-28T18:45:00"]);
-
-echo json_encode($events-feed);
-?>
