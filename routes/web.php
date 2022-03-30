9039043817d221eec6db26b68e90590dddd3812c
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-29 18:08:50 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
-use App\Http\Controllers\CalendarEventsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\InvoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,50 +17,12 @@ use App\Http\Controllers\CalendarEventsController;
 |
 */
 
+Route::resource('/user', UserController::class);
+Route::resource('/manufacturer', ManufacturerController::class);
+Route::resource('/equipment', EquipmentController::class);
+Route::resource('/invoice', InvoiceController::class);
+
+
 Route::get('/', function () {
     return view('welcome');
-});
-
-URL::forceScheme('https');
-
-Route::get('/', function () {
-    return view('todos');
-});
-
-Route::get('/events-feed', function () {
-    return view('calendar');
-});
-
-Route::get('/', function () {
-    return view('board');
-});
-
-Route::get('/db-test', function () {
-    try {
-        \DB::connection()->getPDO();
-        $db_name = \DB::connection()->getDatabaseName();
-        echo 'Database Connected: '.$db_name;
-    } catch (\Exception $e) {
-        echo 'None';
-    }
-});
-
-Route::get('/db-test', function () {
-    try {         
-         echo \DB::connection()->getDatabaseName();     
-    } catch (\Exception $e) {
-          echo 'None';
-    }
-});
-
-Route::get('/db_migrate', function () {
-    Artisan::call('migrate');
-    echo Artisan::output();
-});
-
-Route::resource('/todos', TodoController::class);
-Route::resource('/calendarevents', CalendarEventsController::class);
-
-Route::fallback(function () {
-    return view('fallback');
 });
