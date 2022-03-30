1b48813867828e50406545107b6dc6ef73d16744
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-29 23:50:37 -0700

    Create ManufactureForm.php

---
+++ app/Forms/ManufactureForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+use Kris\LaravelFormBuilder\Field;
+
+
+class ManufactureForm extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('name', Field::TEXT, [
+                'rules' => 'required',
+                'label' => 'Brand Name'
+            ])
+            ->add('tech_email', Field::EMAIL, [
+                'rules' => 'required',
+                'label' => 'Tech Support E-Mail'
+            ])
+            ->add('tech_phone', Field::TEL, [
+                'rules' => 'required',
+                'label' => 'Tech Support Phone Number'
+            ])
+            ->add('sales_email', Field::EMAIL, [
+                'rules' => 'required',
+                'label' => 'Sales Support E-Mail'
+            ])
+            ->add('sales_phone', Field::TEL, [
+                'rules' => 'required',
+                'label' => 'Sales Support Phone Number'
+            ])
+            ->add('submit', 'submit',[
+                'label' => 'Submit'
+            ]);
+    }
+}
