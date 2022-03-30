c5a878806999198dc2276458fb96f36841a009e1
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-29 18:33:53 -0700

    Create 2022_03_29_033235_create_equipment_table.php

---
+++ database/migrations/2022_03_29_033235_create_equipment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->string("price");
+            $table->string("ghz");
+            $table->string("ram");
+            $table->enum('category', ['desktop', 'laptop', 'tablet']);
+            $table->foreignId('manufacturer_id')->constrained("manufacturer");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+};
