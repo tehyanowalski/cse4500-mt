d072566abd851306be7cb5817d01c83f6cea617b
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-29 22:27:48 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ManufactureController;
 use App\Http\Controllers\EquipmentController;
-use App\Http\Controllers\InvoiceController;
-
+use App\Http\Controllers\PurchasesController;
+use App\Http\Controllers\Notes;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,13 +17,28 @@ use App\Http\Controllers\InvoiceController;
 |
 */
 
-Route::resource('/user', UserController::class);
-Route::resource('/manufacturer', ManufacturerController::class);
+Route::resource('/customer', CustomerController::class);
+Route::resource('/manufacture', ManufactureController::class);
 Route::resource('/equipment', EquipmentController::class);
-Route::resource('/invoice', InvoiceController::class);
-
+Route::resource('/purchases', PurchasesController::class);
+Route::resource('/notes', NotesController::class);
 
 URL::forceScheme('https');
+
+
+Route::get('/db-migrate', function () {
+    Artisan::call('migrate');
+    echo Artisan::output();
+});
+
+Route::get('/db-test', function () {
+    try {         
+         echo \DB::connection()->getDatabaseName();     
+    } catch (\Exception $e) {
+          echo 'None';
+    }
+});
+
 
 Route::get('/', function () {
     return view('welcome');
