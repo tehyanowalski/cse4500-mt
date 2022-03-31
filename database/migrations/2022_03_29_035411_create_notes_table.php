f2f6c0325d45c63a35c0454bdc4b59678119966e
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-30 20:31:45 -0700

    Update 2022_03_29_035411_create_notes_table.php

--- database/migrations/2022_03_29_035411_create_notes_table.php
+++ database/migrations/2022_03_29_035411_create_notes_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string("service");
             $table->string("software");
             $table->string("content");
-            $table->foreignId('equipment_id')->constrained("equipment")->onDelete('cascade');;
+            $table->foreignId('equipment_id')->constrained("equipments")->onDelete('cascade');;
         });
     }
 
