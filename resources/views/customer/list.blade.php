5dc7d30bf44588a145a502c4427a44f467014209
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-30 23:06:51 -0700

    Update list.blade.php

--- resources/views/customer/list.blade.php
+++ resources/views/customer/list.blade.php
@@ -9,18 +9,20 @@
 @section('content')
 <div class="card">
   <div class="card-body">
-    <table id="table" class="table table-bordered">
+    <table id="table" class="table table-bordered sortable">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Name</th><th style="width: 40px">Action</th>
+          <th style="width: 10px">#</th><th>Name</th><th style="width: 40px">Phone Number</th><th style="width: 40px">Email</th>
         </tr>
       </thead>
       <tbody>
-        @foreach($customers AS $customer)
+        @foreach($customer AS $customer)
         <tr>
           <td>{{ $customer->id }}</td>
           <td>{{ $customer->fullname }}</td>
-          <td><a class="btn btn-default btn-sm" href="{{ route('customer.show',['customer'=>$customer->id]) }}">View</a></td>
+          <td>{{ $customer->phonenumber }}</td>
+          <td>{{ $customer->email}}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('customer.show',['customer'=>$customer->id]) }}" style="float:right;">View</a></td>
         </tr>
         @endforeach
       </tbody>
@@ -28,4 +30,13 @@
   </div>
 </div>
 <a href="{{ route('customer.create') }} " class="btn btn-primary" >Create</a>
+
+
+
+@stop
+
+
+
+@section('js')
+<script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
 @stop
