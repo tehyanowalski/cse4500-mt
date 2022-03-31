1681daeeea7d24b9fe318a11b016608faf427475
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-30 22:42:40 -0700

    Update ManufacturerForm.php

--- app/Forms/ManufacturerForm.php
+++ app/Forms/ManufacturerForm.php
@@ -11,26 +11,27 @@ class ManufacturerForm extends Form
     public function buildForm()
     {
         $this
-            ->add('name', Field::TEXT, [
+            add('name', Field::TEXT, [
                 'rules' => 'required',
                 'label' => 'Brand Name'
             ])
-            ->add('tech_email', Field::EMAIL, [
+            ->add('tech_email', Field::TEXT, [
                 'rules' => 'required',
                 'label' => 'Tech Support E-Mail'
             ])
-            ->add('tech_phone', Field::TEL, [
+            ->add('tech_phone', Field::NUMBER, [
                 'rules' => 'required',
                 'label' => 'Tech Support Phone Number'
             ])
-            ->add('sales_email', Field::EMAIL, [
+            ->add('sales_email', Field::TEXT, [
                 'rules' => 'required',
                 'label' => 'Sales Support E-Mail'
             ])
-            ->add('sales_phone', Field::TEL, [
+            ->add('sales_phone', Field::NUMBER, [
                 'rules' => 'required',
                 'label' => 'Sales Support Phone Number'
             ])
+            
             ->add('submit', 'submit',[
                 'label' => 'Submit'
             ]);
