65c89baae73e12acf9775d150fb2463b4ccf6440
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-30 20:12:28 -0700

    Update EquipmentController.php

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -17,7 +17,7 @@ class EquipmentController extends Controller
      */
     public function index()
     {
-        $equipment = Equipment::all();
+        $equipments = Equipment::all();
         return view('equipment.list', compact('equipments'));
     }
 
