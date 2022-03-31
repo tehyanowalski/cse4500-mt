414753725d7e8eedfbac9966c242e3f4fd80b409
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-30 23:17:27 -0700

    Update and rename 2022_03_29_033056_create_customers_table.php to 2022_03_29_033056_create_customer_table.php

--- database/migrations/2022_03_29_033056_create_customer_table.php
+++ database/migrations/2022_03_29_033056_create_customer_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('customer', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('fullname');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('customer');
     }
 };
