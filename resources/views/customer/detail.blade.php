a3a5df3233a2a7a8f73025d0b52ad063332c900e
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-30 19:28:32 -0700

    Update detail.blade.php

--- resources/views/customer/detail.blade.php
+++ resources/views/customer/detail.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Customer')
+@section('title', 'Customers')
 
 @section('content_header')
     <h1>Customer</h1>
@@ -21,15 +21,37 @@
             <dd class="col-sm-9">{{ $customer->phonenumber }}</dd>
         </dl>
     </div>
-    <span style="float:right;">
-        <a href="{{ route('customer.create') }} " class="btn btn-warning">Update</a>
+    <span style="float:right; margin-bottom: 10px">
+        <a href="{{ route('customer.edit', ['customer'=>$customer->id]) }} " class="btn btn-warning">Update</a>
         <a href="{{ route('customer.destroy',['customer'=>$customer->id]) }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('submit-form').submit();">
         Delete</a>
-        <form id="submit-form" action="{{ route('customer.destroy',['customer'=>$customer->id]) }}" method="POST" class="hidden">
+     <form id="submit-form" action="{{ route('customer.destroy',['customer'=>$customer->id]) }}" method="POST" class="hidden">
             @csrf
             @method('DELETE')
         </form>
     </span>
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">ID</th>
+          <th>Items</th>
+          <th>Total</th>
+          <th>Purchase Date</th>
+          <th style="width: 40px">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($customer->purchases AS $purchases)
+        <tr>
+          <td>{{ $purchases->id }}</td>
+          <td>{{ $purchases->number_of_items() }}</td>
+          <td>${{ $purchases->total_price() }}</td>
+          <td>{{ $purchases->purchase_date }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('purchases.show',['purchases'=>$purchases->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
   </div>
 </div>
 @stop
