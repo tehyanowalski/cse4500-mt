53cc6dc16181a055722ac2b299e3fc9299c0b6be
Tehya Nowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-05-22 21:17:18 -0700

    Update ManufacturerController.php

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -2,95 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Manufacture;
 use Illuminate\Http\Request;
-use App\Models\Manufacturer;
-use Kris\LaravelFormBuilder\FormBuilder;
-use App\Forms\ManufacturerForm;
+use Illuminate\Validation\Rule;
 
-class ManufacturerController extends Controller
+class ManufactureController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        $manufacturer = Manufacturer::all();
-        return view('manufacturer.list', compact('manufacturer'));
+        $manufactures = Manufacture::all();
+        return view('manufacture.index',compact('manufactures'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(FormBuilder $formBuilder)
+    public function create()
     {
-        $form = $formBuilder->create(ManufacturerForm::class, [
-            'method' => 'POST',
-            'url' => route('manufacturer.store')
+        return view('manufacture.create');
+    }
+
+    public function store(Request $request)
+    {
+      $validated = $request->validate([
+        'name' => 'required|unique:manufactures,name'
+      ]);
+      $manufacture = Manufacture::create($request->all());
+      return redirect()->route('manufactures.show',compact('manufacture'));
+    }
+
+    public function show(Manufacture $manufacture)
+    {
+        return view('manufacture.show',compact('manufacture'));
+    }
+
+    public function edit(Manufacture $manufacture)
+    {
+        return view('manufacture.edit',compact('manufacture'));
+    }
+
+    public function update(Request $request, Manufacture $manufacture)
+    {
+        $validated = $request->validate([
+           'name' => ['required',Rule::unique('manufactures','name')->ignore($manufacture->id)]
         ]);
-        return view('manufacturer.create', compact('form'));
+        $manufacture->fill($request->all())->save();
+        return $this->show($manufacture);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(FormBuilder $formBuilder)
+    public function destroy(Manufacture $manufacture)
     {
-        $form = $formBuilder->create(ManufacturerForm::class);
-        $form->redirectIfNotValid();
-        Manufacturer::create($form->getFieldValues());
-        return $this->index();
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $manufacturer = Manufacturer::find($id);
-        return view('manufacturer.details', compact('manufacturer'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-       //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    { 
         //
     }
 }
